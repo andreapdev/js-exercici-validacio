6880f7dbcc270f3b031ae94e81d8da535a1b440e
andrea penso <andreap.dev@gmail.com>
2020-09-08 12:10:37 +0200

    Login Validation 1
    Login Validation with 3 functions:
    -email.oninput and pass.oninput compare value with regular expressions
    - validateLogin() called onsubmit, is only true if email AND password are valid
    Issue: email error triggered password invalid-feedback as well
    Fixed: keeping email input and label in a different div from password input and label prevents bubbling

--- js-login/js/index.js
+++ js-login/js/index.js
@@ -1,25 +1,42 @@
 
 //2. Validate login form: email standard format, email and password required
 
+let email = document.getElementById("inputEmail");
+let pass = document.getElementById("inputPassword");
+let emailValid=false;
+let passValid=false;
+const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const passFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+//Email validation
+email.oninput= function(){
+    let emailValue = email.value;
+    if (emailValue.match(mailFormat)) {
+        email.className="form-control is-valid";
+        emailValid=true;
+    }else{
+        email.className="form-control is-invalid";
+        emailValid=false; 
+    }
+}
+
+//Password validation
+pass.oninput= function(){
+    let passValue = pass.value;
+    if (passValue.match(passFormat)) {
+        pass.className="form-control is-valid";
+        passValid=true;
+    }else{
+        pass.className="form-control is-invalid";
+        passValid=false; 
+    }
+}
+
+//Onsubmit validation - only if email and password are valid!
 function validateLogin() {
-  let email = document.forms["form-login"]["inputEmail"].value;
-  let pass = document.forms["form-login"]["inputPassword"].value;
-  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  const passFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
- 
-  if (email.match(mailFormat)&&pass.match(passFormat)) {
-    email.className="form-control is-valid"
-    pass.className="form-control is-valid"
-    return true;
-  }else{
-    if(email.match(mailFormat)==null){
-        email.className="form-control is-invalid"
+    if(emailValid==true&&passValid==true){
+        return true;
+    }else{
+        return false;
     }
-    if(pass.match(passFormat)==null){
-        pass.className="form-control is-invalid"   
-    }
-    return false;
-  }
-};
-
-
+ };
--- js-search/js/index.js
+++ js-search/js/index.js
@@ -1,15 +1,14 @@
 //1. Validate search form: cannot be empty, requires >3 letters
 
-//Real time validation
-    let search = document.getElementById("inputSearch");
-    search.oninput= function(){
-        let searchValue = search.value;
-        if (searchValue.length>=3) {
-            search.className="form-control is-valid";
-            searchValid=true;
-        }else{
-            search.className="form-control is-invalid";
-            searchValid=false; 
-        }  
-    }
-
+//Real time validation + onsumbit!
+let search = document.getElementById("inputSearch");
+search.oninput= function(){
+    let searchValue = search.value;
+    if (searchValue.length>=3) {
+        search.className="form-control is-valid";
+        searchValid=true;
+    }else{
+        search.className="form-control is-invalid";
+        searchValid=false; 
+    }  
+}
